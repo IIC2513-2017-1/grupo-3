f7224fe73791c6805cbadb5ed0727b8c5afa7114
Federico Kunze <fekunze@uc.cl>
2017-04-21 08:41:58 -0300

    validations

--- app/models/cook.rb
+++ app/models/cook.rb
@@ -1,2 +1,13 @@
 class Cook < ApplicationRecord
+
+  validates :firstname,  presence: true, length: { maximum: 25 }
+  validates :lastname,  presence: true, length: { maximum: 25 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+
+  has_many :dishes
+
 end
--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,2 +1,7 @@
 class Dish < ApplicationRecord
+  validates :price, presence: true
+  validates :name, presence: true, length: { maximum: 25 }
+  validates :description, presence: true, length: { maximum: 200 }
+
+  has_many :categories
 end

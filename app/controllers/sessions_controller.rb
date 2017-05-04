e2a52ac22aab8399dbf13c264dfb9ecc11f48b8b
Federico Kunze <fekunze@uc.cl>
2017-05-04 16:03:29 -0300

    Checkbox remember me

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,9 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-
     if user&.authenticate(params[:session][:password])
       log_in user
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to root_path, notice: 'Login successful.'
 
     else

f3ebef0239a4685bf46d8dcb736628bfa730111e
gantunes <gantunes@uc.cl>
2017-05-11 12:34:35 -0300

    seeds implementation

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(first_name: 'Federico', last_name: 'Kunze', email: '',
-            password: '', role: 'admin')
+User.create(first_name: 'Federico', last_name: 'Kunze', email: 'fekunze@uc.cl',
+            password: '123123', role: 'admin')
 User.create(first_name: 'Gustavo', last_name: 'Antunes', email: 'gantunes@uc.cl',
-            password: '', role: 'admin')
+            password: '123123', role: 'admin')
+User.create(first_name: 'Gordon', last_name: 'Ramsay', email: 'gordon_ramsay@gmail.com',
+            password: '123123', role: 'cook')
+User.create(first_name: 'Jamie', last_name: 'Oliver', email: 'jamie_oliver@gmail.com',
+            password: '123123', role: 'cook')

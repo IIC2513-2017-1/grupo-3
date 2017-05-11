7b070be5c2878071b7f627fe76a0f19a248724d5
Federico Kunze <fekunze@uc.cl>
2017-05-11 12:03:07 -0300

    activation link issue solved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,13 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.where(activated: FILL_IN)
+    @users = User.all
   end
 
   # GET /users/1
   # GET /users/1.json
   def show
       @user = User.find(params[:id])
-      redirect_to root_url and return unless FILL_IN
   end
 
   # GET /users/new

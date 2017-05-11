374c33b0d649e232a8c1b45f849a05ab549a7f91
Federico Kunze <fekunze@uc.cl>
2017-05-11 11:10:52 -0300

    Edited Activation Issue secrets.yml values

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,20 +31,14 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.assets.raise_runtime_errors = true
-
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000', port: 3000 }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000'}
   config.action_mailer.delivery_method = :smtp
 # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
-   :authentication       => "plain",
-   :openssl_verify_mode  => 'none',
-   :enable_starttls_auto => true,
-
+   :user_name            => Rails.application.secrets.gmail_username,
+   :password             => Rails.application.secrets.gmail_password,
   }
 
   # Print deprecation notices to the Rails logger.

b26745e8815ab9e8156ac74d0b3707b14cd30d72
gantunes <gantunes@uc.cl>
2017-05-22 20:42:56 -0400

    user profile & index carousel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,16 +28,14 @@ function closeNav() {
 }
 
 function readURL(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-
-            reader.onload = function (e) {
-                $('#avatar-upload')
-                    .attr('src', e.target.result)
-                    .width(320)
-                    .height(320);
-            };
-
-            reader.readAsDataURL(input.files[0]);
-        }
-    }
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      $('#avatar-upload')
+      .attr('src', e.target.result)
+      .width(320)
+      .height(320);
+    };
+    reader.readAsDataURL(input.files[0]);
+  }
+}

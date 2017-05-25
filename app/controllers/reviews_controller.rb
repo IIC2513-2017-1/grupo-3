701cc236a9c02d32d7e9726089763a60f632ad6f
Federico Kunze <fekunze@uc.cl>
2017-05-25 17:45:36 -0400

    forks radio button

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -31,9 +31,10 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @review.user_id = current_user.id
     @review.dish_id = current_dish.id
+    @dish = Dish.find(@review.dish_id)
     respond_to do |format|
       if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
+        format.html { redirect_to @dish, notice: 'Review was successfully created.' }
         format.json { render :show, status: :created, location: @review }
       else
         format.html { render :new }
@@ -45,9 +46,10 @@ class ReviewsController < ApplicationController
   # PATCH/PUT /reviews/1
   # PATCH/PUT /reviews/1.json
   def update
+    @dish = Dish.find(@review.dish_id)
     respond_to do |format|
       if @review.update(review_params)
-        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
+        format.html { redirect_to @dish, notice: 'Review was successfully updated.' }
         format.json { render :show, status: :ok, location: @review }
       else
         format.html { render :edit }

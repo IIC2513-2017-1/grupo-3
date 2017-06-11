ed33288ccf7fa37e8900642608e77629c4deef49
gantunes <gantunes@uc.cl>
2017-06-11 13:56:07 -0400

    Changes in app info view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,13 +20,11 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("sidenav").style.width = "200px";
-    document.getElementById("main").style.marginLeft = "200px";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
     document.getElementById("sidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
 }
 
 function readURL(input) {

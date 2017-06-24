8ffdb29f2a8c64aacf1a7f6525e9bd8f1eb270e1
Federico Kunze <fekunze@uc.cl>
2017-06-24 18:06:48 -0400

    Forks bug solved

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,8 @@ class Review < ApplicationRecord
 
   validates :body, length: { maximum: 500 }
   validates :title, length: { maximum: 45 }
-  validates :forks, presence: true, allow_blank: true, :inclusion => { :in => [0,5] }
+
+  validates :forks, presence: true, allow_blank: true, :inclusion => { :in => [0, 1, 2, 3, 4, 5] }
   belongs_to :user
   belongs_to :dish
 
